f11ad50d8135ab09196bb8ec62871353f5b64dae
mcaubreydare <mcaubreydare@gmail.com>
2023-02-23 10:47:04 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_sign - function that prints the sign of a number
+ *
+ * @n: Parameter description
+ *
+ * Return: (1)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else if (n < 0)
+
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+
+	
+	}
+
+	return (1);
+
+}
