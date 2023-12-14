b5b7f6c133ba7c222ca035880ae2f3f9273aea89
mcaubreydare <mcaubreydare@gmail.com>
2023-12-14 04:55:10 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the main function is put
+ * Return: the programme returns 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
