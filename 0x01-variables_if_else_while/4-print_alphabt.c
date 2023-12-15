24d06631ddc4a77cb41c2cad79025b8aaad303e3
mcaubreydare <mcaubreydare@gmail.com>
2023-12-15 07:41:13 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - still using putchar
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
